df83e6b6bc0f510cc06fa99a5be577a2ca9546bb
gdiazs <diazgbs@gmail.com>
2019-01-23 11:46:58 -0600

    Commons context added

---
+++ sample-commons/src/main/java/io/gdiazs/sample/commons/services/CommonsService.java
@@ -0,0 +1,12 @@
+package io.gdiazs.sample.commons.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CommonsService {
+
+	
+	public void commonsServicePing() {
+		System.out.println("Mensaje de CommonsService");
+	}
+}
--- sample-web/src/main/java/io/gdiazs/sample/controllers/HomeController.java
+++ sample-web/src/main/java/io/gdiazs/sample/controllers/HomeController.java
@@ -2,23 +2,27 @@ package io.gdiazs.sample.controllers;
 
 
 
-import javax.inject.Inject;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import io.gdiazs.othercontext.services.DefaultService;
+import io.gdiazs.sample.commons.services.CommonsService;
 
 @Controller
 public class HomeController {
 	
-	@Inject
-	DefaultService defaultService;
+	@Autowired
+	private DefaultService defaultService;
+	
+	@Autowired
+	private CommonsService commonsService;
 
-	@RequestMapping(method = RequestMethod.GET, value = "/home")
+	@RequestMapping(method = RequestMethod.GET, value = "")
 	public String index() {
 		defaultService.sayHello();
+		commonsService.commonsServicePing();
 		
 		return "index";
 	}
